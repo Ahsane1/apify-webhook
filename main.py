70e3992b7cb914b822d4755c7a1ec92ef3f19827
Ahsane1 <ahsanomer30@gmail.com>
2025-08-12 18:38:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ async def receive_from_clay(request: Request):
             CUSTOM_FIELDS["person1_job_title"]: job_title,
             CUSTOM_FIELDS["person1_location"]: location,
             CUSTOM_FIELDS["person1_linkedin"]: linkedin,
-            CUSTOM_FIELDS["person1_phone"]: phone
+            CUSTOM_FIELDS["person1_work_email"]: work_email
         }
         await update_org_fields(org_id, update_fields)
         person_number = 1
