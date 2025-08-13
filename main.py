f8140ed64eba537d1a0a7b4e7123d2748630ac68
Ahsane1 <ahsanomer30@gmail.com>
2025-08-13 15:25:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,40 @@ APIFY_TOKEN = "apify_api_yUm3GvrXmoeG33CxHA1CeZWARHXaWj2EjfvM"
 header_of_apify = {
     "Authorization": f"Bearer {APIFY_TOKEN}"
 }
-headers_of_clay = {
-    "x-clay-webhook-auth": "7fd35c803b295103e323"
+#headers_of_clay = { #usa
+ #   "x-clay-webhook-auth": "7fd35c803b295103e323" 
+#}
+headers_of_clay = { #aus
+    "x-clay-webhook-auth": "53d5870039705a75b900"
 }
-url_of_clay_wehook = "https://api.clay.com/v3/sources/webhook/pull-in-data-from-a-webhook-e87962f1-f38a-4c93-a700-c8eb99682e5c"
 
+#url_of_clay_wehook = "https://api.clay.com/v3/sources/webhook/pull-in-data-from-a-webhook-e87962f1-f38a-4c93-a700-c8eb99682e5c"#usa
+url_of_claywebhook = "https://api.clay.com/v3/sources/webhook/pull-in-data-from-a-webhook-53d0e72f-6502-41d4-9cb9-68d34c57fbe1" #aus
 # Fetch dataset items from Apify
 dataset_base_url = "https://api.apify.com/v2/datasets"
 PIPEDRIVE_TOKEN = "27d9e9ab8c16e564839bd2e7701cdae8df870092"
 PIPEDRIVE_BASE_URL = "https://api.pipedrive.com/v1"
 email_custom_code = "d3ea4f66c0020e31d8f6e9b7019004332a17c250" #custom fields in pipedrive
 phone_custom_code = "e245c6f274d1c1023f3e3b3a161575f43a332f53"
+
+CUSTOM_FIELDS = {
+    # Person 1
+    "person1_full_name": "06741854f635ba133020a3e707a4e6b92a2cecad",
+    "person1_job_title": "f44a5919612a8e3a720e7144d668d1d2630e7dee",
+    "person1_location": "9a7fa237317c5ce6535640c9f080c5f4f19e26c4",
+    "person1_linkedin": "3dc35ee613649e02012925ba320a56e456baa0b1",
+    "person1_work_email": "8732b372dcf333690790184258d0a9b3c9fee20b",
+    "person1_phone": "3d21362c58f26e0c565677041010fb41bbde3bf1",
+
+    # Person 2
+    "person2_full_name": "66b796a45695e9dbdd50dd8392905e5e0aa01861",
+    "person2_job_title": "0c0e4fd1738d92f00485521db8f4d9486f3fe7a6",
+    "person2_location": "5fcebc80bcc2393f48b6207982d70ad3c7968e69",
+    "person2_linkedin": "4970a94767d1ee1dad1fc97a9d6b972e736fdb61",
+    "person2_phone": "353c5ef54c699b1959ae32c9ce7fd2a4b8abb5ab",
+    "person2_work_email": "25cf48c981ab9ab316519347a6835ece494d2c93"
+}
+
 
 app = FastAPI()
 
@@ -43,7 +66,7 @@ async def send_to_clay(session, item):
         print("Status:", res.status)
 
 # Remove duplicates and upload to Clay
-async def upload_to_clay(session, dataset_items):
+async def check_uniqueness_and_send_to_clay(session, dataset_items):
     seen = set()
     unique_items = []
     for item in dataset_items:
@@ -88,26 +111,6 @@ async def create_lead(title, org_id):
             return data.get("data", {})
 
 
-
-
-# Custom field codes mapping
-CUSTOM_FIELDS = {
-    # Person 1
-    "person1_full_name": "06741854f635ba133020a3e707a4e6b92a2cecad",
-    "person1_job_title": "f44a5919612a8e3a720e7144d668d1d2630e7dee",
-    "person1_location": "9a7fa237317c5ce6535640c9f080c5f4f19e26c4",
-    "person1_linkedin": "3dc35ee613649e02012925ba320a56e456baa0b1",
-    "person1_work_email": "8732b372dcf333690790184258d0a9b3c9fee20b",
-    "person1_phone": "3d21362c58f26e0c565677041010fb41bbde3bf1",
-
-    # Person 2
-    "person2_full_name": "66b796a45695e9dbdd50dd8392905e5e0aa01861",
-    "person2_job_title": "0c0e4fd1738d92f00485521db8f4d9486f3fe7a6",
-    "person2_location": "5fcebc80bcc2393f48b6207982d70ad3c7968e69",
-    "person2_linkedin": "4970a94767d1ee1dad1fc97a9d6b972e736fdb61",
-    "person2_phone": "353c5ef54c699b1959ae32c9ce7fd2a4b8abb5ab",
-    "person2_work_email": "25cf48c981ab9ab316519347a6835ece494d2c93"
-}
 
 async def get_all_organizations():
     async with aiohttp.ClientSession() as session:
@@ -217,7 +220,7 @@ async def receive_from_clay(request: Request):
 
 
 # Webhook endpoint
-@app.post("/")
+@app.post("/apify")
 async def handle(request: Request):
     body = await request.json()
     dataset_id = body.get("datasetId")
@@ -229,7 +232,7 @@ async def handle(request: Request):
 
     async with aiohttp.ClientSession() as session:
         dataset_items = await fetch(session, dataset_url)
-        unique_items = await upload_to_clay(session, dataset_items)
+        unique_items = await check_and_send_to_clay(session, dataset_items)
         return {"status": "Processed successfully"}
 
 
